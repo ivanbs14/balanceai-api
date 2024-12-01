d640853c1b93983523e1e5310a3ad752fa448e10
Ivan Barbosa <ivanbarbosa@MacBook-Pro-de-Ivan.local>
2024-12-01 01:55:52 -0300

    feat: create router for getTotalExpensesAndInvestmentsForPreviousMonth

--- src/transation/transation.controller.ts
+++ src/transation/transation.controller.ts
@@ -20,6 +20,12 @@ export class TransationController {
     return this.transationService.findAll();
   }
 
+  // Get total expenses for the previous month
+  @Get('expenses/previous-month/:userId/:month')
+  async getTotalExpensesForPreviousMonth(@Param('userId') userId: string, @Param('month') month: string) {
+    return this.transationService.getTotalExpensesAndInvestmentsForPreviousMonth(userId, month);
+  }
+
   // Get by User ID
   @Get('user/:userId')
   async findByUserId(@Param('userId') userId: string) {
--- src/transation/transation.service.ts
+++ src/transation/transation.service.ts
@@ -17,6 +17,65 @@ export class TransationService {
     return this.prisma.transation.findMany();
   }
 
+  // Get Total Expenses for Previous Month
+  async getTotalExpensesAndInvestmentsForPreviousMonth(userId: string, month: string) {
+    const year = new Date().getFullYear();
+    const startDate = new Date(`${year}-${month}-01T00:00:00.000Z`);
+    const endDate = new Date(startDate);
+    endDate.setMonth(endDate.getMonth() + 1);
+
+    const expenses = await this.prisma.transation.aggregate({
+      _sum: {
+        amount: true,
+      },
+      where: {
+        userId,
+        type: 'EXPENSE',
+        Date: {
+          gte: startDate,
+          lt: endDate,
+        },
+      },
+    });
+
+    const investments = await this.prisma.transation.aggregate({
+      _sum: {
+        amount: true,
+      },
+      where: {
+        userId,
+        type: 'INVESTMENT',
+        Date: {
+          gte: startDate,
+          lt: endDate,
+        },
+      },
+    });
+
+    const deposits = await this.prisma.transation.aggregate({
+      _sum: {
+        amount: true,
+      },
+      where: {
+        userId,
+        type: 'DEPOSIT',
+        Date: {
+          gte: startDate,
+          lt: endDate,
+        },
+      },
+    });
+
+    const balance = Number(deposits._sum.amount) - (Number(expenses._sum.amount) + Number(investments._sum.amount));
+
+    return {
+      totalExpenses: Number(expenses._sum.amount) || 0,
+      totalInvestments: Number(investments._sum.amount) || 0,
+      totalDeposits: Number(deposits._sum.amount) || 0,
+      balance: balance || 0,
+    };
+  }
+
   // Get by ID
   async findOne(id: string) {
     return this.prisma.transation.findUnique({ where: { id } });
