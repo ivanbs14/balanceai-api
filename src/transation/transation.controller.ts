51fd968d2fc8e99900035f15a74d30c3c5a4b683
Ivan Barbosa <ivanbarbosag@gmail.com>
2025-03-05 14:30:41 -0300

    refact: styless

--- src/transation/transation.controller.ts
+++ src/transation/transation.controller.ts
@@ -8,43 +8,36 @@ import { CreateTransationDto } from './dto/create-transation.dto';
 export class TransationController {
   constructor(private readonly transationService: TransationService) {}
 
-  // Create
   @Post()
   async create(@Body() data: CreateTransationDto) {
     return this.transationService.create(data);
   }
 
-  // Get All
   @Get()
   async findAll() {
     return this.transationService.findAll();
   }
 
-  // Get total expenses for the previous month
   @Get('expenses/previous-month/:userId/:month')
   async getTotalExpensesForPreviousMonth(@Param('userId') userId: string, @Param('month') month: string) {
     return this.transationService.getTotalExpensesAndInvestmentsForPreviousMonth(userId, month);
   }
 
-  // Get by User ID
   @Get('user/:userId/:month')
   async findByUserIdAndMonth(@Param('userId') userId: string, @Param('month') month: string) {
     return this.transationService.findByUserIdAndMonth(userId, month);
   }
 
-  // Get by ID
   @Get(':id')
   async findOne(@Param('id') id: string) {
     return this.transationService.findOne(id);
   }
 
-  // Update
   @Put(':id')
   async update(@Param('id') id: string, @Body() data: Prisma.TransationUpdateInput) {
     return this.transationService.update(id, data);
   }
 
-  // Delete
   @Delete(':id')
   async delete(@Param('id') id: string) {
     return this.transationService.delete(id);
