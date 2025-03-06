cd73b5edd44e2da464348613947be7b88b4e1ff4
Ivan Barbosa <ivanbarbosag@gmail.com>
2025-03-06 16:55:22 -0300

    Fix: Cors 1.2

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,11 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: ["http://localhost:3000", "http://localhost:3001"],
+    origin: [
+      "http://localhost:3000",
+      "http://localhost:3001",
+      "https://balance-2olb8gbo5-ivan-barbosas-projects.vercel.app/",
+    ],
     methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
     allowedHeaders: "Content-Type, Accept",
     credentials: true,
