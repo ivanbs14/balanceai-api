06930ecdb787f586023b159e5767987a6abdda5e
Ivan Barbosa <ivanbarbosag@gmail.com>
2025-03-06 10:50:36 -0300

    Fix: Use dynamic port for Heroku

--- src/main.ts
+++ src/main.ts
@@ -4,15 +4,17 @@ import { AppModule } from "./app.module";
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  // Habilitar CORS
   app.enableCors({
-    // origin: ["http://localhost:4000", "http://localhost:3001"], // Adicione os origins necessários
     origin: ["http://localhost:3000"],
     methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
     allowedHeaders: "Content-Type, Accept",
     credentials: true,
   });
 
-  await app.listen(4000);
+  const PORT = process.env.PORT || 4000; // Usa a porta do Heroku ou 4000 para local
+  await app.listen(PORT);
+
+  console.log(`🚀 Server is running on port ${PORT}`);
 }
+
 bootstrap();
